e9f0de3fe5407792e7fab0836834fca2a91bfc2b
trilobit-gmbh <trilobit-gmbh@users.noreply.github.com>
2023-01-09 11:34:51 +0100

    Feature/contao5 compatibility (#4)
    * adjust dependencies and constraints
    update and run php cs fixer
    
    * adjust for contao 5
    
    * some fixes
    
    * some fixes

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,12 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * @copyright  trilobit GmbH
  * @author     trilobit GmbH <https://github.com/trilobit-gmbh>
  * @license    LGPL-3.0-or-later
- * @link       http://github.com/trilobit-gmbh/contao-newsaddons-bundle
  */
 
-$GLOBALS['FE_MOD']['news']['newsmenu'] = \Trilobit\NewsaddonsBundle\ModuleNewsMenu::class;
-$GLOBALS['FE_MOD']['news']['newsarchive'] = \Trilobit\NewsaddonsBundle\ModuleNewsArchive::class;
-$GLOBALS['FE_MOD']['news']['newslist'] = \Trilobit\NewsaddonsBundle\ModuleNewsList::class;
+use Trilobit\NewsaddonsBundle\ModuleNewsArchive;
+use Trilobit\NewsaddonsBundle\ModuleNewsList;
+use Trilobit\NewsaddonsBundle\ModuleNewsMenu;
+
+$GLOBALS['FE_MOD']['news']['newsmenu'] = ModuleNewsMenu::class;
+$GLOBALS['FE_MOD']['news']['newsarchive'] = ModuleNewsArchive::class;
+$GLOBALS['FE_MOD']['news']['newslist'] = ModuleNewsList::class;
